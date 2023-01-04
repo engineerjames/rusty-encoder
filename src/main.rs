5bba25ea9dfa564e585bb748d02c0291f9bfde9d
James Armes <jamesleearmes@gmail.com>
2023-01-03 20:57:00 -0700

    Add capability to read text directly from command line

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
+use rusty_encoder::base64encoder;
 use std::env;
 use std::fs;
-use rusty_encoder::base64encoder;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!("Program usage: `rusty-encoder <path_to_file>`");
+    if args.len() < 2 {
+        println!("Program usage: `rusty-encoder -f <path_to_file>`");
+        println!("               `rusty-encoder <text_to_encode>");
         return;
     }
 
-    let file_binary_contents = fs::read(&args[1]).expect("Unable to read file!");
-    let encoded_string = base64encoder::encode(&file_binary_contents);
+    let read_input_from_file = args.contains(&String::from("-f"));
+
+    let encoded_string: String;
+    if read_input_from_file {
+        let file_binary_contents = fs::read(&args[2]).expect("Unable to read file!");
+        encoded_string = base64encoder::encode(&file_binary_contents);
+    } else {
+        let string_as_bytes = args[1].as_bytes().to_vec();
+        encoded_string = base64encoder::encode(&string_as_bytes);
+    }
 
     println!("{}", encoded_string);
 }
