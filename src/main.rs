a7ec558b5acdbd02d780d51ad1d734aa9b66b0fd
James Armes <jamesleearmes@gmail.com>
2023-01-03 19:34:38 -0700

    Simplify using statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::fs;
-
-use rusty_encoder::base64encoder::encode;
+use rusty_encoder::base64encoder;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,7 +11,7 @@ fn main() {
     }
 
     let file_binary_contents = fs::read(&args[1]).expect("Unable to read file!");
-    let encoded_string = encode(&file_binary_contents);
+    let encoded_string = base64encoder::encode(&file_binary_contents);
 
     println!("{}", encoded_string);
 }
